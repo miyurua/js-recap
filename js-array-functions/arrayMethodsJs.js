008e5ffd6f58728d1526f7cd3bcc182c193abedc
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-07-23 20:50:17 +0530

    for each method

--- js-array-functions/arrayMethodsJs.js
+++ js-array-functions/arrayMethodsJs.js
@@ -1,6 +1,17 @@
 const food = ["apple", "carrot", "pear", "banana", "pineapple", "avocado"];
 
 /** Method 01 - forEach
- *
+ * params - call back function
+ * mainly to execute the same code on all the elements in an array
+ * 
+ * output --> 
+apple
+carrot
+pear
+banana
+pineapple
+avocado
  */
-food.forEach();
+food.forEach((item) => {
+  console.log(item);
+});
