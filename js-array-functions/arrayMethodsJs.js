4d7012d468f21fcb9341ea6216a46c5bb59b6bf2
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-07-24 22:47:07 +0530

    sort, filter, concat

--- js-array-functions/arrayMethodsJs.js
+++ js-array-functions/arrayMethodsJs.js
@@ -1,4 +1,5 @@
 const food = ["apple", "carrot", "pear", "banana", "pineapple", "avocado"];
+const people = ["john", "jack", "ava", "bella", "zane"];
 
 /** Method 01 - forEach
  * params - call back function
@@ -26,3 +27,30 @@ const mapFood = food.map((item, key) => {
 });
 
 console.log(mapFood);
+
+/**
+ * Method 03 - sort
+ *
+ * returns an array that's sorted
+ */
+
+const sortFood = food.sort();
+
+console.log(sortFood);
+
+/**
+ * Method 04 - Filter
+ *
+ * returns an array that meets the condition
+ */
+const filterFood = food.filter((item) => item === "apple");
+console.log(filterFood);
+
+/**
+ * Method 05 - Concat
+ *
+ * concatenates 2 arrays
+ */
+const concatFoodPeople = food.concat(people);
+
+console.log(concatFoodPeople);
